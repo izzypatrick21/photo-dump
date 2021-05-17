3dc1c7f3a885f341d16385821af1c47a29b3190f
Patrick Isaiah <patrick21isaiah@gmail.com>
2021-05-17 20:24:55 +0100

    Updates

---
+++ precache-manifest.9857b9fa363964eb769b94936f1d2e45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26c740b65f8147876306208d4743e7e6",
+    "url": "/photo-dump/index.html"
+  },
+  {
+    "revision": "2289d98e4effe64def76",
+    "url": "/photo-dump/static/css/main.b062aa27.chunk.css"
+  },
+  {
+    "revision": "9a1fd04661a66bb95a77",
+    "url": "/photo-dump/static/js/2.9611706c.chunk.js"
+  },
+  {
+    "revision": "f13b8ee62297cdeb55e53ad5d3f0bd7f",
+    "url": "/photo-dump/static/js/2.9611706c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2289d98e4effe64def76",
+    "url": "/photo-dump/static/js/main.f2b4f9a8.chunk.js"
+  },
+  {
+    "revision": "8dab4dde2ebf4adf91a8",
+    "url": "/photo-dump/static/js/runtime-main.e722ac2b.js"
+  }
+]);
